ec3b5d01260d3404a3af8fdad85f12faf949f2c2
Emircan Aydın <emircanaydin973@gmail.com>
2022-08-14 17:13:21 +0300

    inital commit.

---
+++ src/main/java/com/mavi/atlas/MaviAtlasBackend/MaviAtlasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mavi.atlas.MaviAtlasBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaviAtlasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaviAtlasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavi/atlas/MaviAtlasBackend/MaviAtlasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavi.atlas.MaviAtlasBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaviAtlasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
